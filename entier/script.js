4bf6c42b8fbccdb57a56af364b450548d3b146dd
Prince <alvinprinceikalo01021998@gmail.com>
2023-05-12 01:06:32 +0300

    main(modif)+ jscrpte

---
+++ entier/script.js
@@ -0,0 +1,13 @@
+
+  function internet() {
+            var x = document.getElementById("search").value;
+
+            if (x === "dog") {
+                window.open("/index.html");
+            }
+
+            if (x === "cat") {
+                window.open("/customizedalert.html");
+            }
+
+        }
